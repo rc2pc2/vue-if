e2e7dc84ca280dde93596e4788d4db5f3d6b14de
rc2pc2 <petriccarcc@gmail.com>
2023-06-05 10:37:28 +0200

    v-for basic usage example

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ createApp({
     data() {
         return {
             isRaining : false,
+            partecipants : [
+                {
+                    name : 'Ginetta',
+                    role: 'Software Developer',
+                    experience : 5
+                },
+                {
+                    name : 'Ginetto',
+                    role: 'Designer',
+                    experience : 2
+                },
+                {
+                    name : 'Lorenzina',
+                    role: 'Junior Software Developer',
+                    experience : 1
+                },
+
+            ],
         }
     },
     methods: {
